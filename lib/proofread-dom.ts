ba6ce03f52c0bab84b7e87bcdc098df017f2c7b0
Julian Price <github.com@julianprice.org.uk>
2024-05-14 22:15:06 +0100

    Remove console.log

--- lib/proofread-dom.ts
+++ lib/proofread-dom.ts
@@ -41,43 +41,41 @@ class ProofreadDom extends ProofreadTranscript {
   }
 
   handleLoadButtonClick = async (event: Event) => {
-      if (event.type === "click") {
-        console.log("handleEvent click");
-        const el = document.getElementById(this.prefix + "-transcript-url") as HTMLInputElement;
-        console.log("URL=" + el?.value);
-        if (el) {
-          await this.load(el.value);
-          let container = document.getElementById(this.prefix + "-audio") as HTMLAudioElement;
-          container.src = this.getUrl();
-          container.addEventListener("timeupdate", this.handleTimeupdate);
+    if (event.type === "click") {
+      const el = document.getElementById(this.prefix + "-transcript-url") as HTMLInputElement;
+      if (el) {
+        await this.load(el.value);
+        let container = document.getElementById(this.prefix + "-audio") as HTMLAudioElement;
+        container.src = this.getUrl();
+        container.addEventListener("timeupdate", this.handleTimeupdate);
 
-          this.updateLine();
-        }
-      }
-    }
-
-  handleTimeupdate = async (event: Event) => {
-      const audio = event.target as HTMLAudioElement;
-      const currentTime: number = audio.currentTime;
-
-      const beforeMonlogueIndex = this.currentSection;
-      const beforeWordIndex = this.currentWord;
-      const beforeIssBetween = this.isBetween;
-
-      this.setCurrentTime(currentTime);
-      if (beforeMonlogueIndex != this.currentSection ) {
         this.updateLine();
       }
-      else if (beforeWordIndex != this.currentWord || beforeIssBetween != this.isBetween ) {
-        let span = document.getElementById(this.prefix + "-word-" + String(beforeWordIndex) ) as HTMLSpanElement;
-        span.style.setProperty('background-color','','');
+    }
+  }
 
-        if (!this.isBetween) {
-          span = document.getElementById(this.prefix + "-word-" + String(this.currentWord) ) as HTMLSpanElement;
-          span.style.setProperty('background-color','#FFFF00','');
-          //console.log( "Updated Time=" + String(audio.currentTime) + ", Line=" + String(this.currentSection)  + ", Word=" + String(this.currentWord)   + ", isBetween=" + String(this.isBetween) + ", word=" + span.innerText + ", LastEnd=" + String(this.lastEnd)  );
-        }
+  handleTimeupdate = async (event: Event) => {
+    const audio = event.target as HTMLAudioElement;
+    const currentTime: number = audio.currentTime;
+
+    const beforeMonlogueIndex = this.currentSection;
+    const beforeWordIndex = this.currentWord;
+    const beforeIssBetween = this.isBetween;
+
+    this.setCurrentTime(currentTime);
+    if (beforeMonlogueIndex != this.currentSection ) {
+      this.updateLine();
+    }
+    else if (beforeWordIndex != this.currentWord || beforeIssBetween != this.isBetween ) {
+      let span = document.getElementById(this.prefix + "-word-" + String(beforeWordIndex) ) as HTMLSpanElement;
+      span.style.setProperty('background-color','','');
+
+      if (!this.isBetween) {
+        span = document.getElementById(this.prefix + "-word-" + String(this.currentWord) ) as HTMLSpanElement;
+        span.style.setProperty('background-color','#FFFF00','');
+        //console.log( "Updated Time=" + String(audio.currentTime) + ", Line=" + String(this.currentSection)  + ", Word=" + String(this.currentWord)   + ", isBetween=" + String(this.isBetween) + ", word=" + span.innerText + ", LastEnd=" + String(this.lastEnd)  );
       }
     }
+  }
 }
 export default ProofreadDom;
